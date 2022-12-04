f63a10719cd49b825bb61f882c5000fd803ad866
Kyunghoon <kkh9172@naver.com>
2022-12-04 14:05:34 +0900

    20221204 PhotoCell 추가

---
+++ Example/HiddentagUIimages/HIDDENPhotoViewCell.h
@@ -0,0 +1,17 @@
+//
+//  HIDDENPhotoViewCell.h
+//  HiddentagUIimages_Example
+//
+//  Created by 경훈's MacBook on 2022/12/04.
+//  Copyright © 2022 kimkyunghoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HIDDENPhotoViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/HiddentagUIimages/HIDDENPhotoViewCell.m
@@ -0,0 +1,18 @@
+//
+//  HIDDENPhotoViewCell.m
+//  HiddentagUIimages_Example
+//
+//  Created by 경훈's MacBook on 2022/12/04.
+//  Copyright © 2022 kimkyunghoon. All rights reserved.
+//
+
+#import "HIDDENPhotoViewCell.h"
+
+@implementation HIDDENPhotoViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
