7803dad6aa57166f1ea3cc7a8a17ee9bec32c9e7
Kyunghoon <kkh9172@naver.com>
2022-12-04 13:43:03 +0900

    20221204 카메라 UI 넣기 커밋

---
+++ Example/HiddentagUIimages/HIDDENCamerCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  HIDDENCollectionViewCell.m
+//  HiddentagUIimages_Example
+//
+//  Created by 경훈's MacBook on 2022/12/04.
+//  Copyright © 2022 kimkyunghoon. All rights reserved.
+//
+
+#import "HIDDENCamerCollectionViewCell.h"
+
+@implementation HIDDENCamerCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
